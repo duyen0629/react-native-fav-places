a6971cb1c51c1edcf1395ad20cad2867112b9f86
Chloe Pham <chloe.ph2906@gmail.com>
2026-06-21 12:26:31 +1000

    Show map with Map View

--- screens/Map.js
+++ screens/Map.js
@@ -1,14 +1,32 @@
 import MapView, { Marker } from "react-native-maps";
 import { StyleSheet } from "react-native";
+import { useState } from "react";
 
 function Map() {
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
+  function handleSelectLocation(event) {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+    setSelectedLocation({ lat, lng });
+  }
+
   const region = {
     latitude: 37.7749,
     longitude: -122.4194,
     latitudeDelta: 0.01,
     longitudeDelta: 0.01,
   };
-  return <MapView initialRegion={region} style={styles.map}></MapView>;
+  return (
+    <MapView initialRegion={region} style={styles.map} onPress={handleSelectLocation}>
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{ latitude: selectedLocation.lat, longitude: selectedLocation.lng }}
+        />
+      )}
+    </MapView>
+  );
 }
 
 export default Map;

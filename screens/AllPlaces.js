a0350e398d865bd003448ffe9da085f91bac2803
Chloe Pham <chloe.ph2906@gmail.com>
2026-06-21 18:55:04 +1000

    Output a List of Places

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,7 +1,19 @@
 import PlacesList from "../components/Places/PlacesList";
+import { useEffect, useState } from "react";
+import { useIsFocused } from "@react-navigation/native";
 
-function AllPlaces() {
-  return <PlacesList places={[]} />;
+function AllPlaces({ route }) {
+  const isFocused = useIsFocused();
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      const placeData = route.params.placeData;
+      setLoadedPlaces((prevPlaces) => [...prevPlaces, placeData]);
+    }
+  }, [isFocused, route.params]);
+
+  return <PlacesList places={loadedPlaces} />;
 }
 
 export default AllPlaces;
